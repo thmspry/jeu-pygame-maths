4c653196669b52298bce299504a2a5f039d3a356
Thomas PERAY <thomas.peray@etu.univ-nantes.fr>
2021-01-10 14:55:11 +0100

    Pierre qui tourne genre vraiment

--- Projectile.py
+++ Projectile.py
@@ -12,13 +12,17 @@ class Projectile(pygame.sprite.Sprite) :
         self.vy = 0
         self.image = GameConfig.ROCK_IMG
         self.rect = pygame.Rect(self.x, self.y, self.taille[0], self.taille[1])
+        self.delay = 0
 
     def draw(self, window):
         window.blit(self.image, self.rect)
 
     def mouvement(self, vitesse):
         self.rect.x += vitesse * self.direction
-        self.image = pygame.transform.rotate(self.image, -90)
+        self.delay += 1
+        if self.delay == 3:
+            self.image = pygame.transform.rotate(self.image, -90)
+            self.delay = 0
     '''def advance_state(self, next_move):
         if next_move.attack :
             self.vx = self.vx*GameConfig.DT
